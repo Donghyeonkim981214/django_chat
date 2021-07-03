66ba8194af87643332a0678b5233fba96c7eb09f
Donghyeonkim981214 <43703346+Donghyeonkim981214@users.noreply.github.com>
2021-07-04 02:51:59 +0900

    Tutorial Part 2: Implement a Chat Server

---
+++ chat/consumers.py
@@ -0,0 +1,54 @@
+# chat/consumers.py
+import json
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        self.accept()
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+"""
+When a user posts a message, a JavaScript function will transmit the message over WebSocket to a ChatConsumer.
+The ChatConsumer will receive that message and forward it to the group corresponding to the room name.
+Every ChatConsumer in the same group (and thus in the same room) will then receive the message
+from the group and forward it over WebSocket back to JavaScript, where it will be appended to the chat log.
+"""
