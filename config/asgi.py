db4c7623f4cfec10ede21031d1ad829452d0d6ee
Donghyeonkim981214 <43703346+Donghyeonkim981214@users.noreply.github.com>
2021-07-07 06:26:53 +0900

    deploy settin
    restore asgi.py in config folder
    remove routing.py in config folder

--- config/asgi.py
+++ config/asgi.py
@@ -9,9 +9,19 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 # config/asgi.py
 import os
-import django
-from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
-application = get_default_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
