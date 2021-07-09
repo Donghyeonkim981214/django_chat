1eb1f65d5ef579210da19077fcdf01894f23757b
Donghyeonkim981214 <43703346+Donghyeonkim981214@users.noreply.github.com>
2021-07-09 14:48:50 +0900

    change for wss

--- config/settings.py
+++ config/settings.py
@@ -33,14 +33,14 @@ ALLOWED_HOSTS = ['django-chat-with-channels.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'channels',
-    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
+    'chat',
 ]
 
 MIDDLEWARE = [
