c5821896c35767a82660b1bfae0c02e4c9fcc8bc
Donghyeonkim981214 <43703346+Donghyeonkim981214@users.noreply.github.com>
2021-07-10 15:43:51 +0900

    url change

--- config/routing.py
+++ config/routing.py
@@ -3,8 +3,10 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
+from django.core.asgi import get_asgi_application
+
 application = ProtocolTypeRouter({
-    # (http->django views is added by default)
+    "http": get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
-    path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
+    path('', include('chat.urls')),
 ]
